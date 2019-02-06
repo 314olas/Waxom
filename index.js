a24cb015cdb0ad8dd7a65daef8d279d05e463905
Eugen Rud <camcoh.ne@gmail.com>
2019-02-06 15:45:58 +0200

    for lesson

---
+++ index.js
@@ -0,0 +1,40 @@
+
+import collapse from "./src/js/collapse.js";
+//import slick from "./src/js/slick.min.js"
+
+
+$(document).ready(function(){
+  $('.slider-portfolio-template').slick({
+    dots: true
+  });
+  $('.posts-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 690,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
+
